c9e1e53551129619661616dc112bf730f25569c4
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-30 20:23:59 +0500

    Add the component calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.scss';
+import Calculator from './componenets/calculator';
 
 function App() {
   return (
     <div className="App">
       <div className="components">
-        <h1>first component</h1>
+        <Calculator />
       </div>
     </div>
   );
