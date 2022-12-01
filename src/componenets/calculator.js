c49ea3373c1b66e94430db8160b0f8b05d8dc579
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-01 21:32:28 +0500

    Add styling

--- src/componenets/calculator.js
+++ src/componenets/calculator.js
@@ -15,7 +15,7 @@ class Calculator extends Component {
 
   handleBtn = (btn) => {
     this.setState((object) => calculate(object, btn));
-  }
+  };
 
   render() {
     const { total, operation, next } = this.state;
@@ -24,40 +24,168 @@ class Calculator extends Component {
         <div className="btn-container">
           <div className="screen">
             <p>
-              <span>{ total }</span>
-              <span>{ operation }</span>
-              <span>{ next }</span>
+              <span>{total}</span>
+              <span>{operation}</span>
+              <span>{next}</span>
             </p>
           </div>
-          <div className="btn-row">
-            <button id="AC" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>AC</button>
-            <button id="+/-" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>+/-</button>
-            <button id="%" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>%</button>
-            <button id="&divide;" className="btn color" type="button" onClick={(e) => this.handleBtn(e.target.id)}>&divide;</button>
-          </div>
-          <div className="btn-row">
-            <button id="7" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>7</button>
-            <button id="8" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>8</button>
-            <button id="9" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>9</button>
-            <button id="x" className="btn color" type="button" onClick={(e) => this.handleBtn(e.target.id)}>&times;</button>
-          </div>
-          <div className="btn-row">
-            <button id="4" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>4</button>
-            <button id="5" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>5</button>
-            <button id="6" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>6</button>
-            <button id="-" className="btn color" type="button" onClick={(e) => this.handleBtn(e.target.id)}>&minus;</button>
-          </div>
-          <div className="btn-row">
-            <button id="1" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>1</button>
-            <button id="2" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>2</button>
-            <button id="3" className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>3</button>
-            <button id="&#43;" className="btn color" type="button" onClick={(e) => this.handleBtn(e.target.id)}>&#43;</button>
-          </div>
-          <div className="btn-last-row">
-            <button id="0" className="btn zero" type="button" onClick={(e) => this.handleBtn(e.target.id)}>0</button>
-            <button id="." className="btn" type="button" onClick={(e) => this.handleBtn(e.target.id)}>.</button>
-            <button id="&#61;" className="btn color" type="button" onClick={(e) => this.handleBtn(e.target.id)}>&#61;</button>
-          </div>
+
+          <button
+            id="AC"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            AC
+          </button>
+          <button
+            id="+/-"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            +/-
+          </button>
+          <button
+            id="%"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            %
+          </button>
+          <button
+            id="&divide;"
+            className="btn color"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            &divide;
+          </button>
+
+          <button
+            id="7"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            7
+          </button>
+          <button
+            id="8"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            8
+          </button>
+          <button
+            id="9"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            9
+          </button>
+          <button
+            id="x"
+            className="btn color"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            &times;
+          </button>
+
+          <button
+            id="4"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            4
+          </button>
+          <button
+            id="5"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            5
+          </button>
+          <button
+            id="6"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            6
+          </button>
+          <button
+            id="-"
+            className="btn color"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            &minus;
+          </button>
+
+          <button
+            id="1"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            1
+          </button>
+          <button
+            id="2"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            2
+          </button>
+          <button
+            id="3"
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            3
+          </button>
+          <button
+            id="&#43;"
+            className="btn color"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            &#43;
+          </button>
+
+          <button
+            id="0"
+            className="btn zero"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            0
+          </button>
+          <button
+            id="."
+            className="btn"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            .
+          </button>
+          <button
+            id="&#61;"
+            className="btn color"
+            type="button"
+            onClick={(e) => this.handleBtn(e.target.id)}
+          >
+            &#61;
+          </button>
         </div>
       </div>
     );
